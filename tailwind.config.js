6cf86b4c7a362cf96d7f18fce01394a15c08ee55
jesenascop18 <jeasen.tenda@gmail.com>
2023-03-26 01:24:49 +0800

    uts-kelas-senin-jeasen

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['halaman1.html', 'halaman2.html', 'halaman3.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
